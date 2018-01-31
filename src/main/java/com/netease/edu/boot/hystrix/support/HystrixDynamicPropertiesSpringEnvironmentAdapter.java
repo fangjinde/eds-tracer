e53e2a0712cd8e14ddf386a2cf8a1e0483ccbdea
hzfjd <hzfjd@corp.netease.com>
2018-01-31 15:48:03 +0800

    @dev add dynamic for hystrixProps

--- src/main/java/com/netease/edu/boot/hystrix/support/HystrixDynamicPropertiesSpringEnvironmentAdapter.java
+++ src/main/java/com/netease/edu/boot/hystrix/support/HystrixDynamicPropertiesSpringEnvironmentAdapter.java
@@ -5,198 +5,250 @@ import com.netflix.hystrix.strategy.properties.HystrixDynamicProperty;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.context.environment.EnvironmentChangeEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.Environment;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * 适配到spring environment
+ *
  * @author hzfjd
  * @create 17/12/24
  */
-public class HystrixDynamicPropertiesSpringEnvironmentAdapter implements HystrixDynamicProperties,EnvironmentAware{
+public class HystrixDynamicPropertiesSpringEnvironmentAdapter
+        implements HystrixDynamicProperties, EnvironmentAware, ApplicationListener<EnvironmentChangeEvent> {
 
-    public HystrixDynamicPropertiesSpringEnvironmentAdapter(){
+    public HystrixDynamicPropertiesSpringEnvironmentAdapter() {
     }
 
     private static Environment s_environment;
 
     private static Logger logger = LoggerFactory.getLogger(HystrixDynamicPropertiesSpringEnvironmentAdapter.class);
 
-    @Override
-    public HystrixDynamicProperty<String> getString(final String name,final String fallback) {
+    private static ConcurrentHashMap<String, AbstractHystrixDynamicProperty> props = new ConcurrentHashMap<String, AbstractHystrixDynamicProperty>(
+            200);
 
-        HystrixDynamicProperty<String> hp = new HystrixDynamicProperty<String>() {
+    private abstract class AbstractHystrixDynamicProperty<T> implements HystrixDynamicProperty<T> {
+
+        List<Runnable> callbacks = new ArrayList<Runnable>(5);
+        private String name;
+
+        protected AbstractHystrixDynamicProperty(String name) {
+            this.name = name;
+        }
+
+        public List<Runnable> getCallbacks() {
+            return callbacks;
+        }
+
+        @Override
+        public synchronized void addCallback(Runnable callback) {
+            callbacks.add(callback);
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+    }
+
+    @Override
+    public HystrixDynamicProperty<String> getString(String name, final String fallback) {
+
+        HystrixDynamicProperty oldHp = props.get(name);
+
+        if (oldHp != null) {
+            return oldHp;
+        }
+
+        AbstractHystrixDynamicProperty<String> newhp = new AbstractHystrixDynamicProperty<String>(name) {
 
             @Override
             public String get() {
                 String propValue = null;
                 if (s_environment != null) {
-                    propValue = s_environment.getProperty(name);
+                    propValue = s_environment.getProperty(getName());
                 }
                 if (StringUtils.isBlank(propValue)) {
                     propValue = fallback;
                 }
                 return propValue;
             }
-
-            @Override
-            public String getName() {
-                return name;
-            }
-
-            @Override
-            public void addCallback(Runnable callback) {
-
-            }
         };
 
+        oldHp = props.putIfAbsent(name, newhp);
+        if (oldHp != null) {
+            return oldHp;
+        } else {
+            return newhp;
+        }
 
-
-        return hp;
     }
 
-    @Override public HystrixDynamicProperty<Integer> getInteger(final String name,final Integer fallback) {
+    @Override
+    public HystrixDynamicProperty<Integer> getInteger(String name, final Integer fallback) {
 
+        HystrixDynamicProperty oldHp = props.get(name);
 
+        if (oldHp != null) {
+            return oldHp;
+        }
 
-
-
-        HystrixDynamicProperty<Integer> hp=new HystrixDynamicProperty<Integer>(){
+        AbstractHystrixDynamicProperty<Integer> newhp = new AbstractHystrixDynamicProperty<Integer>(name) {
 
             @Override
             public Integer get() {
-                String propValue=null;
-                if (s_environment!=null){
-                    propValue= s_environment.getProperty(name);
+                String propValue = null;
+                if (s_environment != null) {
+                    propValue = s_environment.getProperty(getName());
                 }
 
-                Integer propIntValue=null;
-                if (StringUtils.isNotBlank(propValue)){
-                    try{
-                        propIntValue= Integer.parseInt(propValue);
-                    }catch (Exception e){
-                        logger.warn("parse property value of ({}) error",name,e);
+                Integer propIntValue = null;
+                if (StringUtils.isNotBlank(propValue)) {
+                    try {
+                        propIntValue = Integer.parseInt(propValue);
+                    } catch (Exception e) {
+                        logger.warn("parse property value of ({}) error", getName(), e);
                     }
                 }
 
-
-                if (propIntValue==null){
-                    propIntValue=fallback;
+                if (propIntValue == null) {
+                    propIntValue = fallback;
                 }
 
                 return propIntValue;
             }
-
-            @Override
-            public String getName() {
-                return name;
-            }
-
-            @Override
-            public void addCallback(Runnable callback) {
-
-            }
         };
-        return hp;
+
+        oldHp = props.putIfAbsent(name, newhp);
+        if (oldHp != null) {
+            return oldHp;
+        } else {
+            return newhp;
+        }
+
     }
 
-    @Override public HystrixDynamicProperty<Long> getLong(final  String name,final Long fallback) {
+    @Override
+    public HystrixDynamicProperty<Long> getLong(String name, final Long fallback) {
 
+        HystrixDynamicProperty oldHp = props.get(name);
 
+        if (oldHp != null) {
+            return oldHp;
+        }
 
-        HystrixDynamicProperty<Long> hp=new HystrixDynamicProperty<Long>(){
+        AbstractHystrixDynamicProperty<Long> newhp = new AbstractHystrixDynamicProperty<Long>(name) {
 
             @Override
             public Long get() {
-                String propValue=null;
-                if (s_environment!=null){
-                    propValue= s_environment.getProperty(name);
+                String propValue = null;
+                if (s_environment != null) {
+                    propValue = s_environment.getProperty(getName());
                 }
 
-                Long propObjectValue=null;
-                if (StringUtils.isNotBlank(propValue)){
-                    try{
-                        propObjectValue= Long.parseLong(propValue);
-                    }catch (Exception e){
-                        logger.warn("parse property value of ({}) error",name,e);
+                Long propObjectValue = null;
+                if (StringUtils.isNotBlank(propValue)) {
+                    try {
+                        propObjectValue = Long.parseLong(propValue);
+                    } catch (Exception e) {
+                        logger.warn("parse property value of ({}) error", getName(), e);
                     }
                 }
 
-
-                if (propObjectValue==null){
-                    propObjectValue=fallback;
+                if (propObjectValue == null) {
+                    propObjectValue = fallback;
                 }
 
                 return propObjectValue;
             }
-
-            @Override
-            public String getName() {
-                return name;
-            }
-
-            @Override
-            public void addCallback(Runnable callback) {
-
-            }
         };
-        return hp;
+
+        oldHp = props.putIfAbsent(name, newhp);
+        if (oldHp != null) {
+            return oldHp;
+        } else {
+            return newhp;
+        }
+
     }
 
-    @Override public HystrixDynamicProperty<Boolean> getBoolean(final String name,final Boolean fallback) {
+    @Override
+    public HystrixDynamicProperty<Boolean> getBoolean(String name, final Boolean fallback) {
 
+        HystrixDynamicProperty oldHp = props.get(name);
 
+        if (oldHp != null) {
+            return oldHp;
+        }
 
-        HystrixDynamicProperty<Boolean> hp=new HystrixDynamicProperty<Boolean>(){
+        AbstractHystrixDynamicProperty<Boolean> newhp = new AbstractHystrixDynamicProperty<Boolean>(name) {
 
             @Override
             public Boolean get() {
-                String propValue=null;
-                if (s_environment!=null){
-                    propValue= s_environment.getProperty(name);
+                String propValue = null;
+                if (s_environment != null) {
+                    propValue = s_environment.getProperty(getName());
                 }
 
-                Boolean propObjectValue=null;
-                if (StringUtils.isNotBlank(propValue)){
-                    try{
-                        propObjectValue= Boolean.parseBoolean(propValue);
-                    }catch (Exception e){
-                        logger.warn("parse property value of ({}) error",name,e);
+                Boolean propObjectValue = null;
+                if (StringUtils.isNotBlank(propValue)) {
+                    try {
+                        propObjectValue = Boolean.parseBoolean(propValue);
+                    } catch (Exception e) {
+                        logger.warn("parse property value of ({}) error", getName(), e);
                     }
                 }
 
-
-                if (propObjectValue==null){
-                    propObjectValue=fallback;
+                if (propObjectValue == null) {
+                    propObjectValue = fallback;
                 }
 
                 return propObjectValue;
             }
-
-            @Override
-            public String getName() {
-                return name;
-            }
-
-            @Override
-            public void addCallback(Runnable callback) {
-
-            }
         };
-        return hp;
+
+        oldHp = props.putIfAbsent(name, newhp);
+        if (oldHp != null) {
+            return oldHp;
+        } else {
+            return newhp;
+        }
+
     }
 
     @Override
     public void setEnvironment(Environment environment) {
 
-        if (s_environment!=null){
+        if (s_environment != null) {
             return;
         }
-        synchronized (HystrixDynamicPropertiesSpringEnvironmentAdapter.class){
-            if (s_environment!=null){
+        synchronized (HystrixDynamicPropertiesSpringEnvironmentAdapter.class) {
+            if (s_environment != null) {
                 return;
             }
-            s_environment=environment;
+            s_environment = environment;
         }
+    }
+
+    @Override
+    public void onApplicationEvent(EnvironmentChangeEvent event) {
+
+        for (AbstractHystrixDynamicProperty<?> hp : props.values()) {
+            for (Runnable runnable : hp.getCallbacks()) {
+                try {
+                    runnable.run();
+                } catch (RuntimeException e) {
+                    logger.error("update Dynamic props callback failed.", e);
+                }
+
+            }
+        }
+
     }
 }
