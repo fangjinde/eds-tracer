0b4eff149a790b7f975b30ed168b7bc1404c2ea6
hzfjd <hzfjd@corp.netease.com>
2018-03-01 21:30:24 +0800

    @dev add base

---
+++ src/main/java/com/netease/edu/boot/trace/configuration/TraceBaseAutoConfiguration.java
@@ -0,0 +1,21 @@
+package com.netease.edu.boot.trace.configuration;/**
+ * Created by hzfjd on 18/3/1.
+ */
+
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author hzfjd
+ * @create 18/3/1
+ */
+@Configuration
+@EnableConfigurationProperties({KafkaProperties.class })
+@ConditionalOnProperty(value = "spring.zipkin.enabled", matchIfMissing = true)
+@AutoConfigureBefore(EduZipkinAutoConfiguration.class)
+public class TraceBaseAutoConfiguration {
+
+}
