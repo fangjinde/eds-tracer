78e9196bda2b6fdb6620144860e2d01ceaf263d6
hzfjd <hzfjd@corp.netease.com>
2018-03-01 21:30:49 +0800

    @dev add pro

---
+++ src/main/java/com/netease/edu/boot/trace/instrument/dubbo/DubboProviderTraceFilter.java
@@ -0,0 +1,34 @@
+package com.netease.edu.boot.trace.instrument.dubbo;
+
+import brave.Span;
+import brave.propagation.TraceContext;
+import com.alibaba.dubbo.common.Constants;
+import com.alibaba.dubbo.common.extension.Activate;
+import com.alibaba.dubbo.rpc.*;
+
+import java.util.Map;
+
+/**
+ * Tracing filter for dubbo provider,returns sr and ss.
+ *
+ * @author Will Tong
+ */
+@Activate(group = Constants.PROVIDER, order = -8990)
+public class DubboProviderTraceFilter implements Filter {
+
+  private DubboServerHandler handler;
+  private TraceContext.Extractor<Map<String, String>> extractor;
+
+  public void setDubboTracing(DubboTracing dubboTracing) {
+    handler = DubboServerHandler.create(dubboTracing, new DubboServerAdapter());
+    extractor = dubboTracing.tracing().propagation().extractor(Map::get);
+  }
+
+  @Override
+  public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+    Span span = handler.handleReceive(extractor);
+    Result rpcResult = invoker.invoke(invocation);
+    handler.handleSend(rpcResult, span);
+    return rpcResult;
+  }
+}
