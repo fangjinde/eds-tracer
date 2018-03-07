08a624b15c2f29a35967bd959a351a1c3e2e851f
hzfjd <hzfjd@corp.netease.com>
2018-03-07 10:15:04 +0800

    @dev add exception stack

--- src/main/java/com/netease/edu/boot/trace/instrument/dubbo/DubboServerParser.java
+++ src/main/java/com/netease/edu/boot/trace/instrument/dubbo/DubboServerParser.java
@@ -1,26 +1,30 @@
 package com.netease.edu.boot.trace.instrument.dubbo;
 
 import brave.SpanCustomizer;
+import com.alibaba.dubbo.common.utils.StringUtils;
 import com.alibaba.dubbo.rpc.Result;
 import com.alibaba.dubbo.rpc.RpcContext;
 
 public class DubboServerParser extends DubboParser {
-  @Override
-  public void request(DubboAdapter adapter, RpcContext rpcContext, SpanCustomizer customizer) {
-    customizer.name(spanName(adapter, rpcContext));
-    String path = adapter.getRemoteAddress(rpcContext);
-    if (path != null) {
-      customizer.tag("consumer.address", path);
-      customizer.tag("consumer.methodName",adapter.getMethodName(rpcContext));
-    }
-  }
 
-  @Override
-  public void response(DubboAdapter adapter, Result rpcResult, SpanCustomizer customizer) {
-    if (!rpcResult.hasException()) {
-      customizer.tag("provider.result", "true");
-    } else {
-      customizer.tag("provider.exception", rpcResult.getException().getMessage());
+    @Override
+    public void request(DubboAdapter adapter, RpcContext rpcContext, SpanCustomizer customizer) {
+        customizer.name(spanName(adapter, rpcContext));
+        String path = adapter.getRemoteAddress(rpcContext);
+        if (path != null) {
+            customizer.tag("consumer.address", path);
+            customizer.tag("consumer.methodName", adapter.getMethodName(rpcContext));
+        }
     }
-  }
+
+    @Override
+    public void response(DubboAdapter adapter, Result rpcResult, SpanCustomizer customizer) {
+        if (!rpcResult.hasException()) {
+            customizer.tag("provider.result", "normal");
+        } else {
+            customizer.tag("provider.result", "exception");
+
+            customizer.tag("provider.exception", StringUtils.toString(rpcResult.getException()));
+        }
+    }
 }
