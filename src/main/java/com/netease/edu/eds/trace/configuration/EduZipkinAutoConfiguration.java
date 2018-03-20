189ef307d7e6b53fbc8cca18a19b8d0fdd7b03e3
hzfjd <hzfjd@corp.netease.com>
2018-03-20 17:56:35 +0800

    @dev add sentry reporter

--- src/main/java/com/netease/edu/eds/trace/configuration/EduZipkinAutoConfiguration.java
+++ src/main/java/com/netease/edu/eds/trace/configuration/EduZipkinAutoConfiguration.java
@@ -1,24 +1,17 @@
 package com.netease.edu.eds.trace.configuration;/**
-
-
-
- /*
- * Copyright 2013-2018 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+                                                 * /* Copyright 2013-2018 the original author or authors. Licensed under
+                                                 * the Apache License, Version 2.0 (the "License"); you may not use this
+                                                 * file except in compliance with the License. You may obtain a copy of
+                                                 * the License at http://www.apache.org/licenses/LICENSE-2.0 Unless
+                                                 * required by applicable law or agreed to in writing, software
+                                                 * distributed under the License is distributed on an "AS IS" BASIS,
+                                                 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+                                                 * implied. See the License for the specific language governing
+                                                 * permissions and limitations under the License.
+                                                 */
 
 import brave.sampler.Sampler;
+import com.netease.edu.eds.trace.sentry.TraceSentryReporter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -37,17 +30,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import zipkin2.Span;
-import zipkin2.reporter.*;
+import zipkin2.reporter.AsyncReporter;
+import zipkin2.reporter.Reporter;
+import zipkin2.reporter.ReporterMetrics;
+import zipkin2.reporter.Sender;
 
 import java.util.concurrent.TimeUnit;
 
 /**
- * {@link org.springframework.boot.autoconfigure.EnableAutoConfiguration Auto-configuration}
- * enables reporting to Zipkin via HTTP. Has a default {@link brave.sampler.Sampler} set as
- * {@link org.springframework.cloud.sleuth.sampler.ProbabilityBasedSampler}.
- * The {@link org.springframework.cloud.sleuth.zipkin2.ZipkinRestTemplateCustomizer} allows you to customize the {@link org.springframework.web.client.RestTemplate}
- * that is used to send Spans to Zipkin. Its default implementation - {@link org.springframework.cloud.sleuth.zipkin2.DefaultZipkinRestTemplateCustomizer}
- * adds the GZip compression.
+ * {@link org.springframework.boot.autoconfigure.EnableAutoConfiguration Auto-configuration} enables reporting to Zipkin
+ * via HTTP. Has a default {@link brave.sampler.Sampler} set as
+ * {@link org.springframework.cloud.sleuth.sampler.ProbabilityBasedSampler}. The
+ * {@link org.springframework.cloud.sleuth.zipkin2.ZipkinRestTemplateCustomizer} allows you to customize the
+ * {@link org.springframework.web.client.RestTemplate} that is used to send Spans to Zipkin. Its default implementation
+ * - {@link org.springframework.cloud.sleuth.zipkin2.DefaultZipkinRestTemplateCustomizer} adds the GZip compression.
  *
  * @author Spencer Gibb
  * @see org.springframework.cloud.sleuth.sampler.ProbabilityBasedSampler
@@ -62,25 +58,17 @@ import java.util.concurrent.TimeUnit;
 @Import(ZipkinSenderConfigurationImportSelector.class)
 public class EduZipkinAutoConfiguration {
 
-
-
-
     /**
-     * Accepts a sender so you can plug-in any standard one. Returns a Reporter so you can also
-     * replace with a standard one.
+     * Accepts a sender so you can plug-in any standard one. Returns a Reporter so you can also replace with a standard
+     * one.
      */
     @Bean
     @ConditionalOnMissingBean
-    public Reporter<Span> reporter(
-            ReporterMetrics reporterMetrics,
-            ZipkinProperties zipkin,
-            Sender sender
-    ) {
-        return AsyncReporter.builder(sender)
-                .queuedMaxSpans(1000) // historical constraint. Note: AsyncReporter supports memory bounds
-                .messageTimeout(zipkin.getMessageTimeout(), TimeUnit.SECONDS)
-                .metrics(reporterMetrics)
-                .build(zipkin.getEncoder());
+    public Reporter<Span> reporter(ReporterMetrics reporterMetrics, ZipkinProperties zipkin, Sender sender) {
+        return AsyncReporter.builder(sender).queuedMaxSpans(1000) // historical constraint. Note: AsyncReporter supports
+                                                                  // memory bounds
+                            .messageTimeout(zipkin.getMessageTimeout(),
+                                            TimeUnit.SECONDS).metrics(reporterMetrics).build(zipkin.getEncoder());
     }
 
     @Bean
@@ -90,8 +78,9 @@ public class EduZipkinAutoConfiguration {
     }
 
     @Bean
-    @ConditionalOnMissingBean ReporterMetrics sleuthReporterMetrics() {
-        return new InMemoryReporterMetrics();
+    @ConditionalOnMissingBean
+    ReporterMetrics sleuthReporterMetrics() {
+        return new TraceSentryReporter();
     }
 
     @Configuration
