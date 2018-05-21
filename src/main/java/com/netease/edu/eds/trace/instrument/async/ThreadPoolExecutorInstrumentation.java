086668a0b99a4559009786afdb46e9f7742df849
hzfjd <hzfjd@corp.netease.com>
2018-05-21 21:16:44 +0800

    @dev threadpool later ok

--- src/main/java/com/netease/edu/eds/trace/instrument/async/ThreadPoolExecutorInstrumentation.java
+++ src/main/java/com/netease/edu/eds/trace/instrument/async/ThreadPoolExecutorInstrumentation.java
@@ -1,22 +1,9 @@
 package com.netease.edu.eds.trace.instrument.async;
 
 import com.netease.edu.eds.trace.spi.TraceAgentInstrumetation;
-import com.netease.edu.eds.trace.support.DefaultAgentBuilderListener;
-import com.netease.edu.eds.trace.support.SpringBeanFactorySupport;
-import net.bytebuddy.agent.builder.AgentBuilder;
-import net.bytebuddy.implementation.MethodDelegation;
-import net.bytebuddy.implementation.bind.annotation.Argument;
-import net.bytebuddy.implementation.bind.annotation.Super;
-import org.springframework.beans.factory.BeanFactory;
 
 import java.lang.instrument.Instrumentation;
 import java.util.Map;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-
-import static net.bytebuddy.matcher.ElementMatchers.isOverriddenFrom;
-import static net.bytebuddy.matcher.ElementMatchers.namedIgnoreCase;
 
 /**
  * @author hzfjd
@@ -26,41 +13,47 @@ public class ThreadPoolExecutorInstrumentation implements TraceAgentInstrumetati
 
     @Override
     public void premain(Map<String, String> props, Instrumentation inst) {
-        new AgentBuilder.Default().type(namedIgnoreCase("java.util.concurrent.ThreadPoolExecutor")).transform((builder,
-                                                                                                               typeDescription,
-                                                                                                               classloader,
-                                                                                                               javaModule) -> builder.method(namedIgnoreCase("execute").and(isOverriddenFrom(Executor.class))).intercept(MethodDelegation.to(TraceInterceptor.class))).with(DefaultAgentBuilderListener.getInstance()).installOn(inst);
-
+        // new AgentBuilder.Default().enableBootstrapInjection(inst, new File("/Users/hzfjd")).ignore(
+        // new
+        // AgentBuilder.RawMatcher.ForElementMatchers(nameStartsWith("net.bytebuddy.").or(nameStartsWith("sun.reflect.")).<TypeDescription>
+        // or(isSynthetic()))).type(namedIgnoreCase("java.util.concurrent.ThreadPoolExecutor")).transform((builder,
+        // typeDescription, classloader, javaModule) ->
+        // builder.method(namedIgnoreCase("execute")).intercept(MethodDelegation.to(TraceInterceptor.class))).with(DefaultAgentBuilderListener.getInstance()).installOn(inst);
+        //
+        //
+        // }
     }
 
-    public static class TraceInterceptor {
-
-        public static void execute(@Argument(0) Runnable command, @Super ExecutorService executorService) {
-            AsyncTracing asyncTracing = null;
-            BeanFactory beanFactory = SpringBeanFactorySupport.getBeanFactory();
-            if (beanFactory != null) {
-                asyncTracing = beanFactory.getBean(AsyncTracing.class);
-            }
-            if (asyncTracing == null) {
-                executorService.execute(command);
-                return;
-            }
-
-            executorService.execute(asyncTracing.tracing().currentTraceContext().wrap(command));
-
-        }
-
-        private static void callSuper(Callable<Void> callable) {
-            try {
-                callable.call();
-            } catch (Exception e) {
-                if (e instanceof RuntimeException) {
-                    throw (RuntimeException) e;
-
-                } else {
-                    throw new RuntimeException("unknown ThreadPoolExecutor execute exception", e);
-                }
-            }
-        }
-    }
+    // public static class TraceInterceptor {
+    //
+    // public static void execute(@Argument(0) Runnable command, @SuperCall Callable<Void> executorService) {
+    // AsyncTracing asyncTracing = null;
+    // BeanFactory beanFactory = SpringBeanFactorySupport.getBeanFactory();
+    // if (beanFactory != null) {
+    // asyncTracing = beanFactory.getBean(AsyncTracing.class);
+    // }
+    // if (asyncTracing == null) {
+    // callSuper(executorService);
+    // return;
+    // }
+    //
+    // callSuper(executorService);
+    //
+    // // executorService.execute(asyncTracing.tracing().currentTraceContext().wrap(command));
+    //
+    // }
+    //
+    // private static void callSuper(Callable<Void> callable) {
+    // try {
+    // callable.call();
+    // } catch (Exception e) {
+    // if (e instanceof RuntimeException) {
+    // throw (RuntimeException) e;
+    //
+    // } else {
+    // throw new RuntimeException("unknown ThreadPoolExecutor execute exception", e);
+    // }
+    // }
+    // }
+    // }
 }
