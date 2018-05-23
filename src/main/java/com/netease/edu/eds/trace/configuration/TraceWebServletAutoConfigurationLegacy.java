4da88d1c0fbaf0b299cc038876fd65ed61653036
hzfjd <hzfjd@corp.netease.com>
2018-05-23 16:54:28 +0800

    @dev prior tracefilter

--- src/main/java/com/netease/edu/eds/trace/configuration/TraceWebServletAutoConfigurationLegacy.java
+++ src/main/java/com/netease/edu/eds/trace/configuration/TraceWebServletAutoConfigurationLegacy.java
@@ -79,7 +79,7 @@ public class TraceWebServletAutoConfigurationLegacy {
                                                                                                         skipUriMatcher,
                                                                                                         webDebugMatcher));
         filterRegistrationBean.setDispatcherTypes(ASYNC, ERROR, FORWARD, INCLUDE, REQUEST);
-        filterRegistrationBean.setOrder(Ordered.LOWEST_PRECEDENCE - 1);
+        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
         return filterRegistrationBean;
     }
 
