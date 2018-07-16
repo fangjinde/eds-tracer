d8255436e25340c29debcf5e58011a6540355c93
hzfjd <hzfjd@corp.netease.com>
2018-07-16 16:06:29 +0800

    @dev refactor log

--- src/main/java/com/netease/edu/eds/trace/support/AgentSupport.java
+++ src/main/java/com/netease/edu/eds/trace/support/AgentSupport.java
@@ -2,14 +2,12 @@ package com.netease.edu.eds.trace.support;
 
 import com.netease.edu.eds.trace.core.Invoker;
 import net.bytebuddy.agent.builder.AgentBuilder;
-import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.implementation.bind.annotation.Morph;
 
 import java.io.File;
 import java.lang.instrument.Instrumentation;
 
-import static net.bytebuddy.matcher.ElementMatchers.isSynthetic;
 import static net.bytebuddy.matcher.ElementMatchers.nameStartsWith;
 
 /**
@@ -26,7 +24,7 @@ public class AgentSupport {
      */
     public static AgentBuilder getBootstrapAgentBuilder(Instrumentation inst) {
         return new AgentBuilder.Default().enableBootstrapInjection(inst,
-                                                                   new File(TraceInstrumentationHolder.getBootstrapLibPath())).ignore(new AgentBuilder.RawMatcher.ForElementMatchers(nameStartsWith("net.bytebuddy.").or(nameStartsWith("sun.reflect.")).<TypeDescription> or(isSynthetic())));
+                                                                   new File(TraceInstrumentationHolder.getBootstrapLibPath())).ignore(new AgentBuilder.RawMatcher.ForElementMatchers(nameStartsWith("net.bytebuddy.").or(nameStartsWith("sun.reflect."))));
     }
 
     /**
