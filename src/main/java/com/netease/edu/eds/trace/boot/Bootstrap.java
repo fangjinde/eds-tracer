0b36b4f2d55a1597695d97e2fe86a014e58e6d94
hzfjd <hzfjd@corp.netease.com>
2018-09-25 22:23:05 +0800

    @dev change to applicationContextInitializer to boot, which make spring boot class loader effective

--- src/main/java/com/netease/edu/eds/trace/boot/Bootstrap.java
+++ src/main/java/com/netease/edu/eds/trace/boot/Bootstrap.java
@@ -4,9 +4,12 @@ import java.lang.instrument.Instrumentation;
 import java.util.Map;
 
 /**
+ * we must load instruments after spring boot class loader is set.
+ * 
  * @author hzfjd
  * @create 18/6/28
  **/
+@Deprecated
 public class Bootstrap {
 
     public static void start(Instrumentation instumentation, Map<String, String> props) {
