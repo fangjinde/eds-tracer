c1c4f907a055ea8f3bc50341b80f86ad22e281c6
hzfjd <hzfjd@corp.netease.com>
2019-02-17 20:45:14 +0800

    @dev success for http client proxy to gw then upstream back

--- eds-tracer-core/src/main/java/com/netease/edu/eds/shuffle/core/ShufflePropertiesSupport.java
+++ eds-tracer-core/src/main/java/com/netease/edu/eds/shuffle/core/ShufflePropertiesSupport.java
@@ -5,102 +5,128 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.core.env.Environment;
 
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 在spring容器实例化ConfigurationPropertiesBindingPostProcessor.class前的应用场景，是无法通过获取ShuffleProperties来获取值。典型场景就是在postProcessBeanFactory，
- * * 此时调用，或导致ShuffleProperties对象没有被ConfigurationPropertiesBindingPostProcessor处理。 这些地方改成从Environment中直接获取。
+ * * 此时调用，或导致ShuffleProperties对象没有被ConfigurationPropertiesBindingPostProcessor处理。
+ * 这些地方改成从Environment中直接获取。
  * 
  * @author hzfjd
  * @create 18/7/20
  **/
 public class ShufflePropertiesSupport {
 
-    /**
-     * switch
-     */
-    public static boolean turnOn() {
-        Environment environment = SpringBeanFactorySupport.getEnvironment();
-        String turnOnValue = null;
-        if (environment != null) {
-            turnOnValue = environment.getProperty("shuffle.enable");
-        }
+	/**
+	 * switch
+	 */
+	public static boolean turnOn() {
+		Environment environment = SpringBeanFactorySupport.getEnvironment();
+		String turnOnValue = null;
+		if (environment != null) {
+			turnOnValue = environment.getProperty("shuffle.enable");
+		}
 
-        if (StringUtils.isNotBlank(turnOnValue)) {
-            return Boolean.parseBoolean(turnOnValue);
-        }
+		if (StringUtils.isNotBlank(turnOnValue)) {
+			return Boolean.parseBoolean(turnOnValue);
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    /**
-     * standard env name, default is edu-std.
-     */
-    public static String getStandardEnvName() {
+	/**
+	 * standard env name, default is edu-std.
+	 */
+	public static String getStandardEnvName() {
 
-        Environment environment = SpringBeanFactorySupport.getEnvironment();
-        String standardEnvName = null;
-        if (environment != null) {
-            standardEnvName = environment.getProperty("shuffle.standardEnvName");
-        }
+		Environment environment = SpringBeanFactorySupport.getEnvironment();
+		String standardEnvName = null;
+		if (environment != null) {
+			standardEnvName = environment.getProperty("shuffle.standardEnvName");
+		}
 
-        if (StringUtils.isNotBlank(standardEnvName)) {
-            return standardEnvName;
-        }
+		if (StringUtils.isNotBlank(standardEnvName)) {
+			return standardEnvName;
+		}
 
-        return ShuffleProperties.STANDARD_ENV_NAME;
+		return ShuffleProperties.STANDARD_ENV_NAME;
 
-    }
+	}
 
-    public static int getDelayMSToSendLatter() {
-        ShuffleProperties shuffleProperties = SpringBeanFactorySupport.getBean(ShuffleProperties.class);
-        if (shuffleProperties == null) {
-            return ShuffleProperties.DELAY_MS_TO_SEND_LATTER;
-        }
-        return shuffleProperties.getDelayMSToSendLatter();
-    }
+	public static int getDelayMSToSendLatter() {
+		ShuffleProperties shuffleProperties = SpringBeanFactorySupport
+				.getBean(ShuffleProperties.class);
+		if (shuffleProperties == null) {
+			return ShuffleProperties.DELAY_MS_TO_SEND_LATTER;
+		}
+		return shuffleProperties.getDelayMSToSendLatter();
+	}
 
-    public static List<String> getAnonymousTopicNames() {
-        ShuffleProperties shuffleProperties = SpringBeanFactorySupport.getBean(ShuffleProperties.class);
-        if (shuffleProperties == null) {
-            return Collections.emptyList();
-        }
-        return shuffleProperties.getAnonymousTopicNames();
-    }
+	public static List<String> getAnonymousTopicNames() {
+		ShuffleProperties shuffleProperties = SpringBeanFactorySupport
+				.getBean(ShuffleProperties.class);
+		if (shuffleProperties == null) {
+			return Collections.emptyList();
+		}
+		return shuffleProperties.getAnonymousTopicNames();
+	}
 
-    /**
-     * queue expire period in test env. default is 3d.
-     */
-    public static long getTestEnvQueueExpirePeriod() {
-        Environment environment = SpringBeanFactorySupport.getEnvironment();
-        String testEnvQueueExpirePeriodValue = null;
-        if (environment != null) {
-            testEnvQueueExpirePeriodValue = environment.getProperty("shuffle.testEnvQueueExpirePeriod");
-        }
+	/**
+	 * queue expire period in test env. default is 3d.
+	 */
+	public static long getTestEnvQueueExpirePeriod() {
+		Environment environment = SpringBeanFactorySupport.getEnvironment();
+		String testEnvQueueExpirePeriodValue = null;
+		if (environment != null) {
+			testEnvQueueExpirePeriodValue = environment
+					.getProperty("shuffle.testEnvQueueExpirePeriod");
+		}
 
-        if (StringUtils.isNotBlank(testEnvQueueExpirePeriodValue)) {
-            return Long.parseLong(testEnvQueueExpirePeriodValue);
-        }
+		if (StringUtils.isNotBlank(testEnvQueueExpirePeriodValue)) {
+			return Long.parseLong(testEnvQueueExpirePeriodValue);
+		}
 
-        return ShuffleProperties.DEFAULT_TEST_ENV_QUEUE_EXPIRE_PERIOD;
+		return ShuffleProperties.DEFAULT_TEST_ENV_QUEUE_EXPIRE_PERIOD;
 
-    }
+	}
 
-    /**
-     * queue message ttl in test env. default is 3h.
-     */
-    public static long getTestEnvQueueMessageTtl() {
-        Environment environment = SpringBeanFactorySupport.getEnvironment();
-        String testEnvQueueMessageTtlValue = null;
-        if (environment != null) {
-            testEnvQueueMessageTtlValue = environment.getProperty("shuffle.testEnvQueueMessageTtl");
-        }
+	/**
+	 * queue message ttl in test env. default is 3h.
+	 */
+	public static long getTestEnvQueueMessageTtl() {
+		Environment environment = SpringBeanFactorySupport.getEnvironment();
+		String testEnvQueueMessageTtlValue = null;
+		if (environment != null) {
+			testEnvQueueMessageTtlValue = environment
+					.getProperty("shuffle.testEnvQueueMessageTtl");
+		}
 
-        if (StringUtils.isNotBlank(testEnvQueueMessageTtlValue)) {
-            return Long.parseLong(testEnvQueueMessageTtlValue);
-        }
+		if (StringUtils.isNotBlank(testEnvQueueMessageTtlValue)) {
+			return Long.parseLong(testEnvQueueMessageTtlValue);
+		}
 
-        return ShuffleProperties.DEFAULT_TEST_ENV_QUEUE_MESSAGE_TTL;
-    }
+		return ShuffleProperties.DEFAULT_TEST_ENV_QUEUE_MESSAGE_TTL;
+	}
+
+	public static Map<String, String> getHttpClientHostRewriteTo() {
+		ShuffleProperties shuffleProperties = SpringBeanFactorySupport
+				.getBean(ShuffleProperties.class);
+		if (shuffleProperties == null) {
+			return Collections.EMPTY_MAP;
+		}
+
+		List<ShuffleProperties.HttpClientRewriteRule> httpClientRewriteRules = shuffleProperties
+				.getHttpClientRewriteRules();
+
+		Map<String, String> map = new LinkedHashMap<>();
+		httpClientRewriteRules.stream()
+				.forEach((httpClientRewriteRule) -> map.put(
+						httpClientRewriteRule.getMatch(),
+						httpClientRewriteRule.getRouteToUri()));
+
+		return map;
+	}
 
 }
--- eds-tracer-core/src/main/java/com/netease/edu/eds/shuffle/instrument/http/HttpClientShuffleInstrumentation.java
+++ eds-tracer-core/src/main/java/com/netease/edu/eds/shuffle/instrument/http/HttpClientShuffleInstrumentation.java
@@ -3,22 +3,32 @@ package com.netease.edu.eds.shuffle.instrument.http;
 import brave.Span;
 import brave.Tracer;
 import brave.Tracing;
+import com.netease.edu.eds.shuffle.core.ShufflePropertiesSupport;
 import com.netease.edu.eds.trace.core.Invoker;
 import com.netease.edu.eds.trace.instrument.http.httpclient.HttpRequestBypassSupport;
 import com.netease.edu.eds.trace.support.AbstractTraceAgentInstrumetation;
+import com.netease.edu.eds.trace.support.SpringBeanFactorySupport;
+import com.netease.edu.eds.trace.utils.PropagationUtils;
 import com.netease.edu.eds.trace.utils.SpanUtils;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.implementation.bind.annotation.*;
 import net.bytebuddy.matcher.ElementMatcher;
 import net.bytebuddy.matcher.ElementMatchers;
 import net.bytebuddy.utility.JavaModule;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 
 import java.lang.reflect.Method;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.regex.Pattern;
 
 import static net.bytebuddy.matcher.ElementMatchers.isOverriddenFrom;
 import static net.bytebuddy.matcher.ElementMatchers.namedIgnoreCase;
@@ -78,20 +88,83 @@ public class HttpClientShuffleInstrumentation extends AbstractTraceAgentInstrume
 				return invoker.invoke(args);
 			}
 
-			if ("echo.fjd.com".equalsIgnoreCase(hostName)) {
-				// http://127.0.0.1:9999
-				HttpHost newHttpHost = new HttpHost("127.0.0.1", 9999,
-						httpHost.getSchemeName());
-				args[0] = newHttpHost;
-				httpRequest.addHeader("Host", hostName);
+			Map<String, String> httpClientHostRewriteToMap = ShufflePropertiesSupport
+					.getHttpClientHostRewriteTo();
 
-				Span span = tracer.currentSpan();
-				SpanUtils.safeTag(span, "HttpClientOriginHost", hostName);
-				SpanUtils.safeTag(span, "HttpClientRewriteTo", "127.0.0.1:9999");
+			for (Map.Entry<String, String> entry : httpClientHostRewriteToMap
+					.entrySet()) {
+				if (Pattern.compile(entry.getKey()).matcher(hostName).find()) {
+
+					ServiceInstance serviceInstance = getServiceInstanceByRoutingToUri(
+							entry.getValue());
+
+					if (serviceInstance == null) {
+						continue;
+					}
+
+					HttpHost newHttpHost = new HttpHost(serviceInstance.getHost(),
+							serviceInstance.getPort(), serviceInstance.getScheme());
+					args[0] = newHttpHost;
+					httpRequest.addHeader("Host", hostName);
+
+					Span span = tracer.currentSpan();
+					SpanUtils.safeTag(span, "HttpClientOriginHost", hostName);
+					SpanUtils.safeTag(span, "HttpClientRewriteTo",
+							serviceInstance.getUri().toString());
+				}
 			}
 
 			return invoker.invoke(args);
 
 		}
+
+		static String GW_SCHEMA = "gw";
+		static String GW_SERVICE_NAME = "eds-sc-gateway";
+		static String GW_URL_PREFIX = GW_SCHEMA + "://" + GW_SERVICE_NAME;
+
+		private static ServiceInstance getServiceInstanceByRoutingToUri(
+				String routingToUrl) {
+
+			String GW_STD = GW_URL_PREFIX + "."
+					+ ShufflePropertiesSupport.getStandardEnvName();
+			String GW_ORIGINNAL = GW_URL_PREFIX + "." + "original";
+			DiscoveryClient discoveryClient = SpringBeanFactorySupport
+					.getBean(DiscoveryClient.class);
+			if (discoveryClient == null) {
+				return null;
+			}
+
+			String serviceId = null;
+
+			List<ServiceInstance> serviceInstances = null;
+			if (GW_STD.equalsIgnoreCase(routingToUrl)) {
+				serviceId = GW_SERVICE_NAME + "."
+						+ ShufflePropertiesSupport.getStandardEnvName();
+
+			}
+			else if (GW_ORIGINNAL.equalsIgnoreCase(routingToUrl)) {
+
+				serviceId = GW_SERVICE_NAME + "." + PropagationUtils.getOriginEnv();
+
+			}
+
+			if (StringUtils.isBlank(serviceId)) {
+				return null;
+			}
+
+			serviceInstances = discoveryClient.getInstances(serviceId.toUpperCase());
+
+			if (CollectionUtils.isEmpty(serviceInstances)) {
+				return null;
+			}
+
+			if (serviceInstances.size() == 1) {
+				return serviceInstances.get(0);
+			}
+
+			int randomIndex = new Random().nextInt(serviceInstances.size());
+			return serviceInstances.get(randomIndex);
+		}
+
 	}
 }
